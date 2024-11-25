57d385ffe127d257aa622a7c6b6c9a72d2a69f95
GHamilton99 <98045033+GHamilton99@users.noreply.github.com>
2024-11-25 13:09:35 -0500

    Update objectoriented.py, adding the rectangle and combining it with face detection using numbers

--- objectoriented.py
+++ objectoriented.py
@@ -1,5 +1,3 @@
-## This will be an object oriented version of the virtual3d game.
-
 print('starting OO virtual3d')
 
 class Facefinder:
@@ -7,6 +5,7 @@ class Facefinder:
 
 	def -- init--(self):
 		print('Facefinder initialize')
+		self.fac_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 		#----------------------------------------------------
 		# main
@@ -14,3 +13,21 @@ class Facefinder:
 
 		ff = Facefinder()
 		print('virtual3d complete')
+	def find_face(self,frame):
+		""" Returns face center(x,y), draws rect on frame """
+
+		# convert to grayscale
+		gray = cv2.cvtcolor(frame,cv2.BGRZGRAY)
+		faces = self.face_cascade.detectmultiscale(gray,minNeighbors = 9)
+		
+
+		faces = self.face_cascade
+		# Draw rectangle 
+		if faces is None:
+			return None
+			bx=by=bw=bh = x,y,w,h
+
+			for (x,y,w,h) in faces 
+				if w>bw: # is current face bigger than biggest found so far
+
+			cv2.rectangle = (frame, (los,by).....3)
